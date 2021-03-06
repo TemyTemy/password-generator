132c27f9349421ea535e6e8ff24ec8a936988e75
Temy <tayolanre2000@yahoo.com>
2021-03-06 17:37:46 +0000

    FEAT: Function declaration for characters length

--- Develop/script.js
+++ Develop/script.js
@@ -11,14 +11,81 @@ var wantsUpperCase;
 var wantNumbers;
 var wantSpecialCharacter;
 
+// Password prompt options
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e',  'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var numericOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+var specialCharacters = ['"', '!', '#', '$', '%', '&', '(', ')', '*',
+                         '+', ',', '-', '.', '/', ':', ';', '<', '=',
+                         '>', '?', '@', '[', '\\', ']',  '^', '_', '`', '{', '|', '}',
+                         '~', '"'
+                        ];
+
+
+
+
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // Initialize password text area
+  passwordText.value = '';
+
+  // passwordText.value = password;
+  if (!getNumberOfCharacter()) {
+     return;
+  };
+
+  confirmIfUserWantsLowerCase();
+  confirmIfUserWantsUpperCase();
+  confirmIfUserWantsNumbers();
+  confirmUserWantsSpecialCharacter();
+
+  if (wantsLowerCase || wantsUpperCase || wantNumbers || wantSpecialCharacter) {
+    var password = generatePassword();
+    // passwordText.value = password;
+    alert(password);
+  } else{
+    alert('You need to chose at least one character type for your password');
+  }
+
+  
 
 }
+
+// Prompt for number of characters
+function getNumberOfCharacter() {
+
+  var howLongIsPassword = window.prompt('How long do you want the password to be ? - Length should be between 8 and 128 characters', 8);
+  if (howLongIsPassword < 8 || howLongIsPassword > 128) {
+    alert('Password should be between 8 and 128 characters long');
+    return false;
+  } else {
+    passwordLength = howLongIsPassword;
+    return true;
+  }
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
