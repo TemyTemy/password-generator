61c999ebe5e2a84e6616f971b3e5d42327a2826b
Temy <tayolanre2000@yahoo.com>
2021-03-06 17:27:35 +0000

    FEAT: Declared global variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,15 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+// Global Variables
+
+var passwordLength = 0;
+var wantsLowerCase;
+var wantsUpperCase;
+var wantNumbers;
+var wantSpecialCharacter;
 
 // Write password to the #password input
 function writePassword() {
